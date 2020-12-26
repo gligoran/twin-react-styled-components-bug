129370bab863dd4aaae1f2878366aac88b4ffb33
Goran Gligorin <goran.gligorin@dlabs.si>
2020-12-26 14:37:43 +0100

    initial commit

---
+++ src/Input.js
@@ -0,0 +1,5 @@
+import tw from 'twin.macro'
+
+const Input = tw.input`shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md`
+
+export default Input
